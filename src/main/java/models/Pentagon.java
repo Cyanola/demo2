38cea0d05f3059255e0c30351bda2aefe1b641da
Ольга Артемова <artemovao771@gmail.com>
2023-10-03 11:30:05 +0300

    cm5trytuyjegfhg

--- src/main/java/models/Pentagon.java
+++ src/main/java/models/Pentagon.java
@@ -1,4 +1,15 @@
 package models;
 
+import javafx.scene.canvas.GraphicsContext;
+
 public class Pentagon extends Shape {
+    @Override
+    public void draw(GraphicsContext gr) {
+
+    }
+
+    @Override
+    public String descriptor() {
+        return null;
+    }
 }
--- src/main/java/models/Triangle.java
+++ src/main/java/models/Triangle.java
@@ -1,4 +1,15 @@
 package models;
 
+import javafx.scene.canvas.GraphicsContext;
+
 public class Triangle extends Shape {
+    @Override
+    public void draw(GraphicsContext gr) {
+
+    }
+
+    @Override
+    public String descriptor() {
+        return null;
+    }
 }
